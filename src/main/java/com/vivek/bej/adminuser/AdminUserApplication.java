380c1e17afe38c1bb24e373c27d8270ec492f3ef
Vivek Pandey <vp1446392@gmail.com>
2023-07-26 00:23:40 +0530

    new Module added for admin User

---
+++ src/main/java/com/vivek/bej/adminuser/AdminUserApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.bej.adminuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminUserApplication.class, args);
+    }
+
+}
