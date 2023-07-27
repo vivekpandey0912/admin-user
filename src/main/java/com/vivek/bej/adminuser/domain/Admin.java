c47b06a4651ada214b4941dc7710c795127aaaa3
Vivek Pandey <vp1446392@gmail.com>
2023-07-28 02:03:44 +0530

    implementing method of service interface

--- src/main/java/com/vivek/bej/adminuser/domain/Admin.java
+++ src/main/java/com/vivek/bej/adminuser/domain/Admin.java
@@ -12,11 +12,14 @@ public class Admin {
 
    @Id
     private String emailId;
+
+   private String password;
     private String name;
     private List<Author> authorList;
 
-    public Admin(String emailId, String name, List<Author> authorList) {
+    public Admin(String emailId, String password, String name, List<Author> authorList) {
         this.emailId = emailId;
+        this.password = password;
         this.name = name;
         this.authorList = authorList;
     }
@@ -30,6 +33,14 @@ public class Admin {
 
     public void setEmailId(String emailId) {
         this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getName() {
@@ -53,11 +64,11 @@ public class Admin {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Admin admin = (Admin) o;
-        return Objects.equals(emailId, admin.emailId) && Objects.equals(name, admin.name) && Objects.equals(authorList, admin.authorList);
+        return Objects.equals(emailId, admin.emailId) && Objects.equals(password, admin.password) && Objects.equals(name, admin.name) && Objects.equals(authorList, admin.authorList);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(emailId, name, authorList);
+        return Objects.hash(emailId, password, name, authorList);
     }
 }
--- src/main/java/com/vivek/bej/adminuser/service/AdminService.java
+++ src/main/java/com/vivek/bej/adminuser/service/AdminService.java
@@ -1,6 +1,14 @@
 package com.vivek.bej.adminuser.service;
 
+import com.vivek.bej.adminuser.domain.Admin;
+import com.vivek.bej.adminuser.domain.Author;
+import com.vivek.bej.adminuser.domain.Book;
+import com.vivek.bej.adminuser.domain.Genre;
+
+
 public interface AdminService {
 
+
+    public Admin addBook(String emailId, Author author, Genre genre, Book book);
 
 }
