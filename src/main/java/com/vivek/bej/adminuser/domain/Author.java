25e70f988db7fc9eacf919811e9a80b35ebf8882
Vivek Pandey <vp1446392@gmail.com>
2023-07-28 01:43:50 +0530

    definition completed for Author.java

--- src/main/java/com/vivek/bej/adminuser/domain/Author.java
+++ src/main/java/com/vivek/bej/adminuser/domain/Author.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
+import java.util.Objects;
 
 @Document
 public class Author {
@@ -14,4 +15,59 @@ public class Author {
     private String name;
     @DBRef
     private List<Genre> genreList;
+
+    public Author(String id, String name, List<Genre> genreList) {
+        this.id = id;
+        this.name = name;
+        this.genreList = genreList;
+    }
+
+    public Author() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Genre> getGenreList() {
+        return genreList;
+    }
+
+    public void setGenreList(List<Genre> genreList) {
+        this.genreList = genreList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id) && Objects.equals(name, author.name) && Objects.equals(genreList, author.genreList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, genreList);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", genreList=" + genreList +
+                '}';
+    }
 }
